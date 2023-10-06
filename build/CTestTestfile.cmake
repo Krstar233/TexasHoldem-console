a3880c69d0f21285869fbaa455522ec14a121873
kr_star <kr_star@qq.com>
2023-10-06 17:55:31 +0800

    添加cmake 文件

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/project/TexasHoldem-console
+# Build directory: D:/project/TexasHoldem-console/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
